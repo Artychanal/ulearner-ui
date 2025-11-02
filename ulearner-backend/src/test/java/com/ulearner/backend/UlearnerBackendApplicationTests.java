190c0e45caf9c7d6fe998552b6a094ae5a73bc1f
Artychanal <128980053+Artychanal@users.noreply.github.com>
2025-11-02 13:46:26 +0200

    chore: add spring boot backend skeleton

---
+++ ulearner-backend/src/test/java/com/ulearner/backend/UlearnerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulearner.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlearnerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
